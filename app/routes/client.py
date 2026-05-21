5b2c7cab61a346a4a8cea74a4220720e501b0ad2
Mikhail <micaaromic@gmail.com>
2026-05-21 12:05:01 +0300

    всё пока работает, щас буду делать фильтры

--- app/routes/client.py
+++ app/routes/client.py
@@ -6,6 +6,8 @@ bp = Blueprint('client_page', __name__)
 
 @bp.route('/client', methods=["GET", "POST"])
 def client():
+    
+# Добавление пользователя
     if request.method == "POST":
         deal_name = request.form.get('deal_name')
         client_name =request.form.get('client_name')
@@ -19,10 +21,25 @@ def client():
         client_info = Client (deal_name=deal_name, client_name=client_name,contacts=contacts, deal_price=deal_price, currency=currency, stage=stage,deadline=deadline)
         db.session.add(client_info)
         db.session.commit()
+    # Поисковая строка
+    search = request.args.get('search-field')
+
+    if search:
+        active_clients =  Client.query.filter(Client.client_name.contains(search), Client.is_active == True).all()
+        inactive_clients =  Client.query.filter(Client.client_name.contains(search), Client.is_active == False).all()
+        return render_template ('clients.html', active_clients=active_clients, inactive_clients=inactive_clients)
     
-    active_clients =  Client.query.filter(Client.is_active == True).all()
-    inactive_clients =  Client.query.filter(Client.is_active == False).all()
-    return render_template ('clients.html', active_clients=active_clients, inactive_clients=inactive_clients)
+    else:
+        active_clients =  Client.query.filter(Client.is_active == True).all()
+        inactive_clients =  Client.query.filter(Client.is_active == False).all()
+    
+    # return render_template ('clients.html', active_clients=active_clients, inactive_clients=inactive_clients)
+# Фильтры
+    currency_filter = request.args.get('currency_filter')
+    dealstage_filter = request.args.get('dealstage_filter')
+    if currency_filter:
+        active_clients =  Client.query.filter(Client.currency ==currency_filter, Client.is_active == True).all()
+        inactive_clients =  Client.query.filter(Client.currency ==currency_filter, Client.is_active == False).all()
 
 
 @bp.route('/client/deactivate/<id>', methods=["POST"])
@@ -53,38 +70,7 @@ def client_del(id):
     db.session.commit()
 
     return redirect ('/client')
-    
 
-# @bp.route('/client/edit/<id>', methods=["POST", "GET"])
-# def edit(id):
-#     client = Client.query.filter_by(id=id).first()
-#     if request.method == "POST":
-        
-#         client.deal_name = request.form.get('deal_name')
-#         client.client_name =request.form.get('client_name')
-#         client.contacts = request.form.get('contacts')
-#         client.deal_price = request.form.get('deal_price')
-#         if not client.deal_price:
-#             client.deal_price = 0
-#         client.currency = request.form.get('currency')
-#         client.stage = request.form.get('stage')
-#         client.deadline = request.form.get('deadline')
-#         db.session.commit()
-#         return redirect('/client')
-
-   
-    return render_template ("client_edit.html", client=client)
-    
-
-@bp.route('/client/search', methods=["GET", "POST"])
-def client_search():
-    if request.method == "POST":
-        client_search
-    
-    active_clients =  Client.query.filter(Client.is_active == True).all()
-    inactive_clients =  Client.query.filter(Client.is_active == False).all()
-    return render_template ('clients.html', active_clients=active_clients, inactive_clients=inactive_clients)
-        
 
         
 
